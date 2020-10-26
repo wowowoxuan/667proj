bd5b92b6a4106f90492d9111e6720495cc4df938
wowowoxuan <wchai01@syr.edu>
2020-10-25 20:04:31 -0400

    update play structure

--- game/connect4.py
+++ game/connect4.py
@@ -158,49 +158,7 @@ class Connect4:
         self.player2play(int(player2input))
         win,player = self.win_check(2)
         return win, player
-    
-print('please input widht:')
-x = input()
-print('please input height')
-y = input()
-while not x.isdigit() or not y.isdigit() or int(x) == 0 or int(y) == 0:
-    print('h or w can not be 0')
-    print('please input widht:')
-    x = input()
-    print('please input height')
-    y = input()
 
-game = Connect4(int(x),int(y))
-while True:
-    
-    win, player = game.player1()
-    if win:
-        if player == 3:
-            print('all win')
-            print(game.chessboard)
-            break
-        print('player' + str(player) +' win!')
-        print(game.chessboard)
-        break
-    boardfullcheck = game.check_full()
-    if boardfullcheck:
-        print('board is full, no one win!')
-        break
-    win, player = game.player2()
-    if win:
-        if player == 3:
-            print('all win')
-            print(game.chessboard)
-            break
-        print('player' + str(player) +' win!')
-        print(game.chessboard)
-        break
-    boardfullcheck = game.check_full()
-    if boardfullcheck:
-        print('board is full, no one win!')
-        break
-
-    
 
 
 
